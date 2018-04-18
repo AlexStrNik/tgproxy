68736b8534556b322188edd2ebc91af3c12225f4
alexa <alexandrstrizjnev@gmail.com>
2018-04-18 21:32:18 +0300

    qqq

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 var socks5 = require('simple-socks');
 var port = process.env.PORT;
-server = socks5.createServer().listen(process.env.PORT,function () {
-    console.log(`SOCKS5 proxy server started on localhost:${process.env.PORT}`);
+server = socks5.createServer().listen(port,function () {
+    console.log(`SOCKS5 proxy server started on localhost:${port}`);
+});
+server.on('proxyConnect', function (info, destination) {
+    console.log('connected to remote server at %s:%d', info.host, info.port);
+
+    destination.on('data', function (data) {
+        console.log(data.length);
+    });
+});
+
+// When data arrives from the remote connection
+server.on('proxyData', function (data) {
+    console.log(data.length);
+});
+
+// When an error occurs connecting to remote destination
+server.on('proxyError', function (err) {
+    console.error('unable to connect to remote server');
+    console.error(err);
+});
+
+// When a proxy connection ends
+server.on('proxyEnd', function (response, args) {
+    console.log('socket closed with code %d', response);
+    console.log(args);
 });
