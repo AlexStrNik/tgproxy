21bb21c48ae1daf9b44ba9b948474969104e38b9
alexa <alexandrstrizjnev@gmail.com>
2018-04-18 21:27:34 +0300

    qqq

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var socks5 = require('simple-socks');
 var port = process.env.PORT;
 server = socks5.createServer().listen(process.env.PORT,function () {
-    console.log('SOCKS5 proxy server started on localhost:process.env.PORT');
+    console.log(`SOCKS5 proxy server started on localhost:${process.env.PORT}`);
 });
